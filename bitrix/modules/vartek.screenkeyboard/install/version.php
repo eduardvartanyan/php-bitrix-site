c3bd1570a9e999b8d7e6a5b2ddc41550c34ca448
evartanyan1989 <evartanyan1989@gmail.com>
2025-11-26 09:09:06 +0800

    version.php — заменил ковычки на двойные

--- bitrix/modules/vartek.screenkeyboard/install/version.php
+++ bitrix/modules/vartek.screenkeyboard/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '1.0.0',
-    'VERSION_DATE' => '2025-11-17 00:00:00',
+    "VERSION"      => "1.0.0",
+    "VERSION_DATE" => "2025-11-17 00:00:00",
 ];
